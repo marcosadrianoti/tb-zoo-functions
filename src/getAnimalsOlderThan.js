f0b622f2fb35fe00dd528b9a1bfe1879641d96a6
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-10-31 16:44:12 -0300

    Projeto Zoo-Functions - Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.find((especie) => especie.name === animal).residents
+    .every((resident) => resident.age >= age);
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
