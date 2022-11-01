1e0c0f7cdf9e020aac0fe6268f0833b732c56db1
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-01 15:39:13 -0300

    Projeto Zoo-Functions - Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,36 +2,25 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   const dEmpls = data.employees;
-  const result = dEmpls.reduce((acc, curr) => curr.managers.some((elem) => elem === id ? true : acc));
-  
-  return result
+  const result = dEmpls.map((empl) => empl.managers);
+  const arr = [];
+  result.forEach((elem) => arr.push(...elem));
+  return arr.some((el) => el === id);
 }
 
-console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
-  // try {
-    // if (!isManager(managerId)){
-    //   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-    // }
-    if (isManager(managerId)) {
-      
-    
-    const arr = []
-    data.employees.forEach(element => {
+  if (isManager(managerId)) {
+    const arr = [];
+    data.employees.forEach((element) => {
       if (element.managers.some((manag) => manag === managerId) === true) {
         arr.push(`${element.firstName} ${element.lastName}`);
       }
     });
     return arr;
-  
-  // } catch (error) {
-  //   return error.message;
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  
 }
-
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
