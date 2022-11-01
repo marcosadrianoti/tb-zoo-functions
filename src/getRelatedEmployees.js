6e74aa7265fbb998881b014dcb57462be6bafe4c
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-01 14:30:49 -0300

    Projeto Zoo-Functions - Requisito 4 - testando

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const dEmpls = data.employees;
+  const result = dEmpls.reduce((acc, curr) => curr.managers.some((elem) => elem === id ? true : acc));
+  
+  return result
 }
 
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  // try {
+    // if (!isManager(managerId)){
+    //   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    // }
+    if (isManager(managerId)) {
+      
+    
+    const arr = []
+    data.employees.forEach(element => {
+      if (element.managers.some((manag) => manag === managerId) === true) {
+        arr.push(`${element.firstName} ${element.lastName}`);
+      }
+    });
+    return arr;
+  
+  // } catch (error) {
+  //   return error.message;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  
 }
+
+console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
