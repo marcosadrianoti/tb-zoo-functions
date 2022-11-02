4ecf14d4dbbf93dcb83650f50ff540b89a38da9b
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-02 18:04:23 -0300

    Projeto Zoo-Functions - Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const specieId = data.employees.find((empl) => empl.id === id).responsibleFor[0];
+
+  const members = data.species.find((specie) => specie.id === specieId).residents;
+
+  let oldestAnimal = [];
+  let age = 0;
+  members.forEach((animal) => {
+    if (animal.age > age) {
+      oldestAnimal = [animal.name, animal.sex, animal.age];
+      age = animal.age;
+    }
+  });
+
+  return oldestAnimal;
 }
 
 module.exports = getOldestFromFirstSpecies;
