bdb9e25949995dca0b0b2233125cee0620e58211
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-02 09:16:13 -0300

    Projeto Zoo-Functions - Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,32 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui...
-  console.log(entrants);
+function countEntrants(arrayEntrants) {
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  arrayEntrants.forEach((element) => {
+    child += element.age < 18 ? 1 : 0;
+    adult += element.age >= 18 && element.age < 50 ? 1 : 0;
+    senior += element.age >= 50 ? 1 : 0;
+  });
+  return {
+    adult,
+    senior,
+    child,
+  };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(arrayEntrants) {
+  if (arrayEntrants === undefined || (Object.keys(arrayEntrants)).length === 0) {
+    return 0;
+  }
+  const entrantsByAge = countEntrants(arrayEntrants);
+  const tablePrices = data.prices;
+  const totalAdult = entrantsByAge.adult * tablePrices.adult;
+  const totalSenior = entrantsByAge.senior * tablePrices.senior;
+  const totalChild = entrantsByAge.child * tablePrices.child;
+
+  return totalAdult + totalSenior + totalChild;
 }
 
 module.exports = { calculateEntry, countEntrants };
