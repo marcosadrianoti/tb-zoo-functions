57330218d6f1930493b7480d8b319e21db70ebd3
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-03 11:50:04 -0300

    Projeto Zoo-Functions - Requisito 11 - Prblemas ainda no teste

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// Retorna um objeto com o id, nome completo e os ids das especies de responsabilidade do colaborador.
+const getDatasEmployee = (employee) => {
+  let objDatas = {};
+  data.employees.find((employeeCurr) => {
+    if (employeeCurr.firstName === employee || employeeCurr.lastName === employee) {
+      objDatas.id = employeeCurr.id;
+      objDatas.fullName = `${employeeCurr.firstName} ${employeeCurr.lastName}`;
+      objDatas.species = employeeCurr.responsibleFor;
+      objDatas.locations = [];
+    }
+  })
+  return objDatas;
+};
+
+// Retorna uma propriedade da espécie.
+const getDataSpecie = (idSpecie, property) => data.species
+  .find((specie) => specie.id === idSpecie)[property];
+
+function getEmployeesCoverage(objEmployee) {
+  const datasEmployee = getDatasEmployee(objEmployee.name);
+  datasEmployee.species.forEach((idSpecie, index) => {
+    datasEmployee.species[index] = getDataSpecie(idSpecie, 'name');
+    datasEmployee.locations[index] = getDataSpecie(idSpecie, 'location');
+  });
+
+  return datasEmployee
 }
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
