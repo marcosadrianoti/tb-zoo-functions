df748e6fec7708b351aca454b7a833e0edaaa3c8
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-03 15:54:56 -0300

    Projeto Zoo-Functions - Requisito 11 - Trabalhando na lista completa

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,10 +1,13 @@
 const data = require('../data/zoo_data');
+const { employees } = data;
 
 // Retorna um objeto com o id, nome completo e os ids das especies de responsabilidade do colaborador.
 const getDatasEmployee = (employee) => {
   let objDatas = {};
   data.employees.find((employeeCurr) => {
-    if (employeeCurr.firstName === employee || employeeCurr.lastName === employee) {
+    if (employeeCurr.firstName === employee
+      || employeeCurr.lastName === employee
+      || employeeCurr.id === employee) {
       objDatas.id = employeeCurr.id;
       objDatas.fullName = `${employeeCurr.firstName} ${employeeCurr.lastName}`;
       objDatas.species = employeeCurr.responsibleFor;
@@ -17,17 +20,40 @@ const getDatasEmployee = (employee) => {
 // Retorna uma propriedade da espécie.
 const getDataSpecie = (idSpecie, property) => data.species
   .find((specie) => specie.id === idSpecie)[property];
+// const getDataSpecie = (ids) => {
+//   ids.map((id) => data.species.find((animal) => animal.id === id).name);
+// }
 
 function getEmployeesCoverage(objEmployee) {
-  const datasEmployee = getDatasEmployee(objEmployee.name);
-  datasEmployee.species.forEach((idSpecie, index) => {
-    datasEmployee.species[index] = getDataSpecie(idSpecie, 'name');
-    datasEmployee.locations[index] = getDataSpecie(idSpecie, 'location');
-  });
+  if (objEmployee !== undefined) {
+    const reference = objEmployee.name ? objEmployee.name : objEmployee.id;
+    const datasEmployee = getDatasEmployee(reference);
+    const arraySpecies = [];
+    const arrayLocation = [];
+    datasEmployee.species.forEach((idSpecie, index) => {
+      // datasEmployee.species[index] = getDataSpecie(idSpecie, 'name');
+      arraySpecies.push(getDataSpecie(idSpecie, 'name'));
+      arrayLocation.push(getDataSpecie(idSpecie, 'location'));
+      // datasEmployee.locations[index] = getDataSpecie(idSpecie, 'location');
+    });
+    datasEmployee.species = arraySpecies;
+    datasEmployee.locations = arrayLocation;
 
-  return datasEmployee
+    return datasEmployee;
+  }
+  const totalList = [];
+  data.employees.forEach((employee) => {
+    const objDatas = {};
+    objDatas.id = employee.id;
+    objDatas.fullName = `${employee.firstName} ${employee.lastName}`;
+    objDatas.species = employee.responsibleFor;
+    objDatas.locations = [];
+    totalList.push(objDatas);
+
+  });
+  return totalList;
 }
 
-console.log(getEmployeesCoverage({ name: 'Sharonda' }));
+console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
