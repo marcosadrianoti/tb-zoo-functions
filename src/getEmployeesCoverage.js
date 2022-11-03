219d035c1b2cc961f775ec53952eb8b29751a606
Marcos Adriano <marcosadriano.ti@gmail.com>
2022-11-03 19:06:57 -0300

    Projeto Zoo-Functions - Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,15 @@
 const data = require('../data/zoo_data');
 
+// Cria objeto do colaborador
+const objCreator = (obj, employeeCurr) => {
+  const myObj = obj;
+  myObj.id = employeeCurr.id;
+  myObj.fullName = `${employeeCurr.firstName} ${employeeCurr.lastName}`;
+  myObj.species = employeeCurr.responsibleFor;
+
+  return myObj;
+};
+
 // Retorna um objeto com o id, nome completo e os ids das especies de responsabilidade do colaborador.
 const getDatasEmployee = (employee) => {
   let objDatas = {};
@@ -9,19 +19,15 @@ const getDatasEmployee = (employee) => {
       || employeeCurr.id === employee) {
       objDatas = objCreator(objDatas, employeeCurr);
     }
-  })
+    return '';
+  });
 
   return objDatas;
 };
 
-// Cria objeto do colaborador
-const objCreator = (obj, employeeCurr) => {
-  obj.id = employeeCurr.id;
-  obj.fullName = `${employeeCurr.firstName} ${employeeCurr.lastName}`;
-  obj.species = employeeCurr.responsibleFor;
-
-  return obj;
-};
+// Retorna uma propriedade da espécie.
+const getDataSpecie = (idSpecie, property) => data.species
+  .find((specie) => specie.id === idSpecie)[property];
 
 // Troca id por name e cria location
 const changeIdAndAddLocation = (obj) => {
@@ -35,35 +41,31 @@ const changeIdAndAddLocation = (obj) => {
   return [arraySpecies, arrayLocation];
 };
 
-// Retorna uma propriedade da espécie.
-const getDataSpecie = (idSpecie, property) => data.species
-  .find((specie) => specie.id === idSpecie)[property];
+// Lança o erro.
+const getError = (objEmployee) => {
+  if (objEmployee.id === 'Id inválido') {
+    throw new Error('Informações inválidas');
+  }
+};
 
+const totalList = [];
 function getEmployeesCoverage(objEmployee) {
   if (objEmployee !== undefined) {
+    getError(objEmployee);
     const reference = objEmployee.name ? objEmployee.name : objEmployee.id;
     const datasEmployee = getDatasEmployee(reference);
-    const arraySpecies = changeIdAndAddLocation(datasEmployee)[0];
-    const arrayLocation = changeIdAndAddLocation(datasEmployee)[1];
-    datasEmployee.species = arraySpecies;
-    datasEmployee.locations = arrayLocation;
-
+    datasEmployee.locations = [...changeIdAndAddLocation(datasEmployee)[1]];
+    datasEmployee.species = [...changeIdAndAddLocation(datasEmployee)[0]];
     return datasEmployee;
   }
-  const totalList = [];
   data.employees.forEach((employee) => {
     let objDatas = {};
     objDatas = objCreator(objDatas, employee);
-    const arraySpecies = changeIdAndAddLocation(objDatas)[0];
-    const arrayLocation = changeIdAndAddLocation(objDatas)[1];
-    objDatas.species = arraySpecies;
-    objDatas.locations = arrayLocation;
+    objDatas.locations = [...changeIdAndAddLocation(objDatas)[1]];
+    objDatas.species = [...changeIdAndAddLocation(objDatas)[0]];
     totalList.push(objDatas);
   });
-
   return totalList;
 }
-
-// console.log(getEmployeesCoverage());
 
 module.exports = getEmployeesCoverage;
